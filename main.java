bc2505701e5077c56259f8c3b2ab30dafa5dae7e
matttttttso <48606684+matttttttso@users.noreply.github.com>
2023-03-28 17:32:43 +0900

    Create main.java

---
+++ main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args ) throws Exception {
+        // スキャナー
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            String token1 = sc.next();
+            String token2 = sc.next();
+            System.out.println("hello = " + token1 + " , world = " + token2);
+        }
+        
+        // リスト
+        List<String> list = new ArrayList<String>();
+        list.add("test1");
+        list.add("test2");
+        list.add("test3");
+        list.add("test4");
+        for (String str : list) {
+            System.out.println("list for loop : " + str);
+        }
+        // リスト stream.filter.collect(Collectors)
+        List<String> filteredList = list.stream()
+					.filter(str -> Objects.equals(str, "test3"))
+					.collect(Collectors.toList());
+		System.out.println("list.stream.filter.collect(Collectors) " + filteredList.size());
+		// リスト stream.filter.findFirst() Optional
+		Optional<String> optStr = list.stream()
+						.filter(str -> Objects.equals(str, "test3"))
+						.findFirst();
+		if (optStr.isPresent()) {
+		    System.out.println("list.stream.filter.findFirst() " + optStr.get());
+    		if (optStr.get().startsWith("t")) {
+    			optStr.get().replace("t", "p");
+			}
+			System.out.println("replace " + optStr.get().replace("t", "p"));
+			System.out.println("replaced but " + optStr.get());
+		}
+    }
+}
